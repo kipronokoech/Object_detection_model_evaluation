c454011d29bb2e592a543cf9bf1381a385f9aade
Kiproon Elijah Koech <kiprono@aims.ac.za>
2023-12-16 11:59:04 +0300

    simplified the code in the script further by removing argparse

--- run_evaluation2.py
+++ run_evaluation2.py
@@ -7,19 +7,6 @@ import matplotlib.pyplot as plt
 loc = os.path.dirname(__file__)
 os.chdir(loc)
 
-import argparse
-parser = argparse.ArgumentParser(description="Some description.")
-
-parser.add_argument("-classes", "--num_classes", type=int, default=1, \
-                    help="Number of classes in your object detection problem")
-parser.add_argument("-conf", "--confidence_threshold", type=float, default=0.5, \
-                    help="Confidence score threshold. Any detection whose \
-                        confidence is under the value given is skipped during evaluation")
-parser.add_argument("-iou", "--iou_threshold", type=float, default=0.5, \
-                    help="IoU threshold. Detections with IoU>threshold is regarded as valid\
-                        else it is false detection.")
-args = parser.parse_args()
-
 
 # Testing the Evaluation Tool on multiple detection-label pairs
 filename = "_MG_3168_10"
@@ -37,7 +24,7 @@ truth2 = truth.copy()
 # truth2[:, :] = truth2[:, :].astype(int)
 
 # Instantiate ConfusionMatrix with your desired thresholds
-confusion_matrix = ConfusionMatrix(num_classes=args.num_classes, confidence_threshold=args.confidence_threshold, iou_threshold=args.iou_threshold)
+confusion_matrix = ConfusionMatrix(num_classes=1, confidence_threshold=0.8, iou_threshold=0.5)
 matrix = confusion_matrix.process_detections(detections= pred2, labels=truth2)
 confusion_matrix.plot_matrix(fontsize=12)
 result = confusion_matrix.compute_tp_fp_fn()
